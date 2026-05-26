2acd32738faf809dbdb3ecc2e80da046d3ec399a
TaeHyunn <xogus998@naver.com>
2026-05-26 15:06:17 +0900

    fix : 수정 후 재업로

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import qs from 'qs';
 
 export const aiClient = axios.create({
-  baseURL: import.meta.env.VITE_AI_API_URL,
+  baseURL: import.meta.env.VITE_AI_API_URL || '/api/ai',
   paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
 });
